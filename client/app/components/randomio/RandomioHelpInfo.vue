3fe3b13e755afdf6a4462996e1d1b50f6499a40d
Steven <stevenld165@gmail.com>
2026-03-13 11:13:24 -0500

    Removed animation of checkboxes and added favicon

--- client/app/components/randomio/RandomioHelpInfo.vue
+++ client/app/components/randomio/RandomioHelpInfo.vue
@@ -7,50 +7,54 @@ const checkboxValues = ref(["1", "2", "3"])
     <div class="flex flex-col gap-4">
       <CardDarkTrans>
         <span class="font-semibold">1. add shows to your list</span>
-        <Button icon="pi pi-plus" label="add show" raised />
+        <NuxtLink to="/edit">
+          <Button icon="pi pi-plus" label="add show" raised />
+        </NuxtLink>
       </CardDarkTrans>
       <CardDarkTrans>
         <span class="font-semibold">2. add some filters</span>
         <div class="rounded-2xl p-4 bg-zinc-50 text-(--prime-dialog-color)">
           <span class="text-lg font-semibold">included seasons:</span>
-          <div class="flex gap-2">
-            <div class="flex items-center gap-1">
-              <Checkbox
-                name="options-example-checkbox"
-                v-model="checkboxValues"
-                value="0"
-                readonly
-              />
-              <label>0</label>
+          <NuxtLink to="/edit">
+            <div class="flex gap-2">
+              <div class="flex items-center gap-1">
+                <Checkbox
+                  name="options-example-checkbox"
+                  v-model="checkboxValues"
+                  value="0"
+                  readonly
+                />
+                <label>0</label>
+              </div>
+              <div class="flex items-center gap-1">
+                <Checkbox
+                  name="options-example-checkbox"
+                  v-model="checkboxValues"
+                  value="1"
+                  readonly
+                />
+                <label>1</label>
+              </div>
+              <div class="flex items-center gap-1">
+                <Checkbox
+                  name="options-example-checkbox"
+                  v-model="checkboxValues"
+                  value="2"
+                  readonly
+                />
+                <label>2</label>
+              </div>
+              <div class="flex items-center gap-1">
+                <Checkbox
+                  name="options-example-checkbox"
+                  v-model="checkboxValues"
+                  value="3"
+                  readonly
+                />
+                <label>3</label>
+              </div>
             </div>
-            <div class="flex items-center gap-1">
-              <Checkbox
-                name="options-example-checkbox"
-                v-model="checkboxValues"
-                value="1"
-                readonly
-              />
-              <label>1</label>
-            </div>
-            <div class="flex items-center gap-1">
-              <Checkbox
-                name="options-example-checkbox"
-                v-model="checkboxValues"
-                value="2"
-                readonly
-              />
-              <label>2</label>
-            </div>
-            <div class="flex items-center gap-1">
-              <Checkbox
-                name="options-example-checkbox"
-                v-model="checkboxValues"
-                value="3"
-                readonly
-              />
-              <label>3</label>
-            </div>
-          </div>
+          </NuxtLink>
         </div>
       </CardDarkTrans>
     </div>
