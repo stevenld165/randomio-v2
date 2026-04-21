360d5b4aead3baeb9e3ac1f17a8b11f6cecd8e20
Steven <stevenld165@gmail.com>
2026-04-20 19:11:39 -0500

    Added ko-fi link

--- client/app/components/navigation/NavigationHeader.vue
+++ client/app/components/navigation/NavigationHeader.vue
@@ -41,26 +41,33 @@ const navMenuRef = useTemplateRef("navMenu")
 </script>
 <template>
   <div class="px-8 py-2 font-bold text-lg flex justify-between items-center">
-    <NuxtLink to="/"><span>randomio</span></NuxtLink>
-    <Button
-      v-if="user"
-      @click="navMenuRef?.toggle"
-      aria-haspopup="true"
-      aria-controls="navigation-menu"
-      variant="text"
-      severity="secondary"
-    >
-      <span class="text-violet-400 flex gap-1 items-center">
-        <span class="font-semibold text-zinc-50">hi</span>{{ user.name }}!
-      </span>
-    </Button>
-    <Menu
-      class="text-base"
-      :model="menuItems"
-      :popup="true"
-      id="navigation-menu"
-      ref="navMenu"
-    />
+    <div class="flex gap-2 items-center">
+      <NuxtLink to="/"><span>randomio</span></NuxtLink>
+      <a href="https://ko-fi.com/rininidev" target="_blank">
+        <Button label="leave a tip on ko-fi!" size="small" rounded />
+      </a>
+    </div>
+    <div>
+      <Button
+        v-if="user"
+        @click="navMenuRef?.toggle"
+        aria-haspopup="true"
+        aria-controls="navigation-menu"
+        variant="text"
+        severity="secondary"
+      >
+        <span class="text-violet-400 flex gap-1 items-center">
+          <span class="font-semibold text-zinc-50">hi</span>{{ user.name }}!
+        </span>
+      </Button>
+      <Menu
+        class="text-base"
+        :model="menuItems"
+        :popup="true"
+        id="navigation-menu"
+        ref="navMenu"
+      />
+    </div>
   </div>
 </template>
 <style lang="scss" scoped></style>
